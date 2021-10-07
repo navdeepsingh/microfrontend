44d13e9d8558a3c9ed9edc1bcbf0bae97dbfdc30
Navdeep singh <navsingh23@WKMZT0526580.global.publicisgroupe.net>
2021-10-07 16:10:12 +0530

    Trigger build app

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <>
-      <h1>Hii from Container!!!!</h1>
+      <h1>Hii from Container</h1>
       <hr />
       <MarketingApp />
     </>
