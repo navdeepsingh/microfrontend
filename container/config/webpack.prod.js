ad4baced5166350563a79116718391e2d4ea2ca5
Navdeep singh <navsingh23@WKMZT0526580.global.publicisgroupe.net>
2021-10-07 16:30:56 +0530

    Updated paths

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
   plugins: [
     new ModuleFederationPlugin({
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
