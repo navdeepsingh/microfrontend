1423f6cdde377c54ccada3ec863f566274d96265
Navdeep singh <navsingh23@WKMZT0526580.global.publicisgroupe.net>
2021-10-13 16:41:26 +0530

    Implementing Multi-Tier Navigation

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, Router } from "react-router-dom";
 import { StylesProvider } from "@material-ui/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
-const App = () => (
+const App = ({ history }) => (
   <StylesProvider>
-    <BrowserRouter>
+    <Router history={history}>
       <Switch>
         <Route path="/pricing" component={Pricing} />
         <Route path="/" component={Landing} />
       </Switch>
-    </BrowserRouter>
+    </Router>
   </StylesProvider>
 );
 
